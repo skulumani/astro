b7a076e01ca664ed3487ebd1c668e6790a727961
Shankar Kulumani <shanks.k@gmail.com>
2017-06-16 10:54:08 -0400

    Sun eci to earth completed

--- planets.py
+++ planets.py
@@ -2,28 +2,42 @@
 Do lots of functions for the planets, sun and/or moon
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-
+import sys 
+sys.path.append('..')
 import numpy as np
-from ..kinematics import attitude
+from tle_predict.kinematics import attitude
+import pdb
 
 def sun_earth_eci(jd):
     """Calcualte the sun vector in the geocentric inertial frame
     """
     twopi = 2 * np.pi
-    rad = np.pi/180
-    
-    # initialize
-    N = JD - 2451545.0
+    deg2rad = np.pi/180
+    Tut1 = ( jd - 2451545.0) / 36525
 
-    meanlong = 280.461 + 0.9856476*N
-    meanlong = attitude.normalize(meanlong, 0, 360)
+    meanlong = attitude.normalize(280.460 + 36000.771 * Tut1, 0, 360.0)
 
-    meananomaly = 357.528 * 0.9856003 * N
-    meananomaly = attitude.normalize(meananomaly * rad, 0, twopi)
+    Ttdb = Tut1 # approximation
+    meananomaly = attitude.normalize(357.5277233 + 35999.05034 * Ttdb, 0, 360)
+    meananomaly = meananomaly * deg2rad
+    eclplong = meanlong + 1.914666471 * np.sin(meananomaly) + 0.019994643 * (
+            np.sin(2 * meananomaly))
+    eclplong = attitude.normalize(eclplong, 0, 360)
+    obliquity = attitude.normalize(23.439291 - 0.0130042 * Ttdb, 0, 360)
+    # convert everything to radians
+    eclplong = eclplong * deg2rad 
+    obliquity = obliquity * deg2rad
+    meanlong = meanlong * deg2rad
+    # right ascension and declination
+    dec = np.arcsin(np.sin(obliquity) * np.sin(eclplong))
 
-    if meananomaly < 0:
-        meananomaly = twopi + meananomaly
+    ra = np.arctan2(np.cos(obliquity)*np.sin(eclplong)/np.cos(dec), 
+            np.cos(eclplong)/np.cos(dec))
+    # magnitude of sun vector 
+    rsun_mag = 1.000140612 - 0.016708617 * np.cos(meananomaly) - (
+            0.000139589 * np.cos(2 * meananomaly))
+    rsun = rsun_mag * np.array([np.cos(eclplong), np.cos(obliquity)*np.sin(eclplong),
+        np.sin(obliquity)*np.sin(eclplong)])
 
-
-
-
+    rsun = rsun * 149597870.7 # output in km
+    return rsun, ra, dec
---
+++ tests/test_planets.py
@@ -0,0 +1,13 @@
+"""test all the functions in planets 
+"""
+import numpy as np
+
+from .. import planets
+
+def test_sun_earth_eci_vallado():
+    jd = 2453827.5
+    expected_rsun = [146186178.0792,28789122.463592, 12481127.00722]
+    expected_ra = 11.140899 * np.pi/180
+    expected_dec = 4.788425 * np.pi/180
+    rsun, ra, dec = planets.sun_earth_eci(jd)
+    np.testing.assert_array_almost_equal(rsun, expected_rsun, decimal=0)
