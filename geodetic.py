1ee6c6d8acea93e76c8001a79f0457ecd2e52f56
Shankar Kulumani <shanks.k@gmail.com>
2017-06-16 17:07:26 -0400

    Add ECEF <> LLA conversions and testing

--- geodetic.py
+++ geodetic.py
@@ -1,6 +1,7 @@
 """Geodetic transformations
 """
 import numpy as np
+from tle_predict.kinematics import attitude
 
 def lla2ecef(lat, lon, alt, r=6378.137, ee=8.1819190842622e-2):
     """Convert latitude, longitude, and altitude to the Earth centered
@@ -31,3 +32,57 @@ def lla2ecef(lat, lon, alt, r=6378.137, ee=8.1819190842622e-2):
     z = ((1 - ee**2) * N + alt) * np.sin(lat)
 
     return np.array([x, y, z])
+
+def ecef2lla(ecef, r=6378.137, ee=8.1819190842622e-2):
+    twopi = 2*np.pi
+    tol = 1e-6
+
+    norm_vec = np.linalg.norm(ecef)
+
+    temp = np.sqrt(ecef[0]**2 + ecef[1]**2)
+    if temp < tol:
+        rtasc = np.sign(ecef[2]) * np.pi * 0.5
+    else:
+        rtasc = np.arctan2(ecef[1], ecef[0])
+
+    lon = rtasc
+    lon = attitude.normalize(lon, 0, 2*np.pi)
+
+    decl = np.arcsin(ecef[2]/ norm_vec)
+    latgd = decl
+
+    # iterate to find geodetic latitude
+    i = 1
+    olddelta = latgd + 10.0
+    while np.absolute(olddelta - latgd) >= tol and i < 10:
+        oldelta = latgd
+        sintemp = np.sin(latgd)
+        c = r / np.sqrt(1.0 - ee**2 * sintemp**2)
+        latgd = np.arctan2(ecef[2] + c * ee**2 * sintemp, temp)
+        i = i +1
+
+    # calculate the height
+    if (np.pi/2 - np.absolute(latgd)) > np.pi/180:
+        hellp = temp/np.cos(latgd) - c
+    else:
+        s = c * (1 - ee**2)
+        hellp = ecef[2] / np.sin(latgd) - s
+
+    latgc = gd2gc(latgd, ee**2)
+    return latgc, latgd, lon, hellp
+
+def gd2gc(latgd, eesqrd=0.081819221456**2):
+    """Only valid for locations on the Earth's surface
+
+    Vallado Example 3-1
+    """
+    latgc = np.arctan( (1 - eesqrd)*np.tan(latgd))
+    return latgc
+
+def gc2gd(latgc, eesqrd=0.081819221456**2):
+    """Only valid for locations on the Earth's surface
+
+    Vallado Example 3-1
+    """
+    latgd = np.arctan(np.tan(latgc) / (1 - eesqrd))
+    return latgd
--- tests/test_geodetic.py
+++ tests/test_geodetic.py
@@ -27,3 +27,34 @@ def test_north_pole_lla2eccef():
     actual_ecef = geodetic.lla2ecef(lat, lon, alt)
     np.testing.assert_array_almost_equal(actual_ecef, expected_ecef)
 
+def test_lla2ecef_vallado():
+    """Example 3-3 from Vallado
+    """
+    latgd = 34.352496 * np.pi/180
+    lon = 46.4464 * np.pi/180
+    hellp = 5085.22 # km
+    expected_ecef = [6524.834, 6862.875, 6448.296] # km
+    actual_ecef = geodetic.lla2ecef(latgd, lon, hellp)
+    np.testing.assert_array_almost_equal(actual_ecef, expected_ecef, decimal=2)
+
+def test_ecef2lla_vallado():
+    """Example 3-3 from Vallado
+    """
+    ecef = [6524.834, 6862.875, 6448.296]
+    # this is in latgd, lon, hellp in km
+    expected_lla = [34.352496*np.pi/180, 46.4464 * np.pi/180, 5085.22]
+    latgc, latgd, lon, hellp = geodetic.ecef2lla(ecef) 
+    np.testing.assert_allclose([latgd, lon, hellp], expected_lla, rtol=1e-3)
+
+def test_gc2gd_vallado():
+    latgc = 34.173429 * np.pi/180
+    expected_latgd = 34.352496 *np.pi/180
+    actual_latgd = geodetic.gc2gd(latgc)
+    np.testing.assert_allclose(actual_latgd, expected_latgd)
+
+def test_gd2gc_vallado():
+    latgd = 34.352496 * np.pi/180
+    expected_latgc = 34.173429 * np.pi/180
+    actual_latgc = geodetic.gd2gc(latgd)
+    np.testing.assert_allclose(actual_latgc, expected_latgc)
+
