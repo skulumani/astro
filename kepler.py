9d3d96655f154efc4a2d14153a7e6fe925a2f80d
Shankar Kulumani <shanks.k@gmail.com>
2017-06-18 11:59:03 -0400

    Vectorize kepler equation solver

--- kepler.py
+++ kepler.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 from ..kinematics import attitude
-
+import pdb
 
 def coe2rv(p, ecc, inc, raan, arg_p, nu, mu):
     """
@@ -124,94 +124,102 @@ def kepler_eq_E(M_in,ecc_in):
        - USAFA Astro 321 LSN 24-25
        - Vallado 3rd Ed pg 72
     """
-    tol = 1e-6
+    tol = 1e-9
     max_iter = 50
+    
+    E_out = []
+    nu_out = []
+    count_out = []
+    
+    if not hasattr(M_in, "__iter__"):
+        M_in = [M_in]
+        ecc_in = [ecc_in]
 
-    M = M_in
-    ecc = ecc_in
-    # eccentricity check
-    """
-        HYPERBOLIC ORBIT
-    """
-    if ecc-1.0  > tol: # eccentricity logic
-        # initial guess
-        if ecc < 1.6: # initial guess logic
-            if M < 0.0 and (M > -np.pi or M > np.pi):
-                E_0 = M - ecc
-            else:
-                E_0 = M + ecc
-            
-        else:
-            if ecc < 3.6 and np.absolute(M) > np.pi:
-                E_0 = M - np.sign(M)*ecc;
-            else:
-                E_0 = M/(ecc-1.0 );
-        
-        # netwon's method iteration to find hyperbolic anomaly
-        count = 1
-        E_1 = E_0 + ( (M - ecc*np.sinh(E_0)+ E_0) / (ecc*np.cosh(E_0) - 1.0 ) )
-        while ((np.absolute(E_1-E_0) > tol ) and ( count <= max_iter )):
-            E_0 = E_1
-            E_1 = E_0 + ( (M - ecc*np.sinh(E_0)+ E_0) / (ecc*np.cosh(E_0) - 1.0 ) )
-            count = count + 1
-        
-        E = E_0
-        # find true anomaly
-        sinv = -( np.sqrt( ecc*ecc-1.0  ) * np.sinh(E_1) ) / ( 1.0  - ecc*np.cosh(E_1) )
-        cosv = ( np.cosh(E_1) - ecc ) / ( 1.0  - ecc*np.cosh(E_1) )
-        nu  = np.arctan2( sinv,cosv );
-    else:
+    for M, ecc in zip(M_in, ecc_in):
+        # eccentricity check
         """
-            PARABOLIC
+            HYPERBOLIC ORBIT
         """
-        if np.absolute(ecc-1.0) < tol: # parabolic logic
-            count= 1
-            
-            S = 0.5  * (np.pi/2 - np.arctan( 1.5 * M ) )
-            W = np.arctan( np.tan( S )**(1.0 /3.0 ) )
-            
-            E = 2.0 *1.0/np.tan(2.0 *W)
-            
-            nu = 2.0  * np.arctan(E)
-        else:
-            """
-                ELLIPTICAl
-            """
-            if  ecc > tol:   # elliptical logic
-                
-                # determine intial guess for iteration
-                if M > -np.pi and (M < 0 or M > np.pi):
+        if ecc-1.0  > tol: # eccentricity logic
+            # initial guess
+            if ecc < 1.6: # initial guess logic
+                if M < 0.0 and (M > -np.pi or M > np.pi):
                     E_0 = M - ecc
                 else:
                     E_0 = M + ecc
-                                    
-                # newton's method iteration to find eccentric anomaly
+                
+            else:
+                if ecc < 3.6 and np.absolute(M) > np.pi:
+                    E_0 = M - np.sign(M)*ecc;
+                else:
+                    E_0 = M/(ecc-1.0 );
+            
+            # netwon's method iteration to find hyperbolic anomaly
+            count = 1
+            E_1 = E_0 + ( (M - ecc*np.sinh(E_0)+ E_0) / (ecc*np.cosh(E_0) - 1.0 ) )
+            while ((np.absolute(E_1-E_0) > tol ) and ( count <= max_iter )):
+                E_0 = E_1
+                E_1 = E_0 + ( (M - ecc*np.sinh(E_0)+ E_0) / (ecc*np.cosh(E_0) - 1.0 ) )
+                count = count + 1
+            
+            E = E_0
+            # find true anomaly
+            sinv = -( np.sqrt( ecc*ecc-1.0  ) * np.sinh(E_1) ) / ( 1.0  - ecc*np.cosh(E_1) )
+            cosv = ( np.cosh(E_1) - ecc ) / ( 1.0  - ecc*np.cosh(E_1) )
+            nu  = np.arctan2( sinv,cosv );
+        else:
+            """
+                PARABOLIC
+            """
+            if np.absolute(ecc-1.0) < tol: # parabolic logic
                 count= 1
-                E_1 = E_0 + ( M - E_0 + ecc*np.sin(E_0) ) / ( 1.0  - ecc*np.cos(E_0) )
-                while (( np.absolute(E_1-E_0) > tol ) and ( count <= max_iter )):
-                    count = count + 1
-                    E_0 = E_1
-                    E_1 = E_0 + ( M - E_0 + ecc*np.sin(E_0) ) / ( 1.0  - ecc*np.cos(E_0) )
                 
-                E = E_0
+                S = 0.5  * (np.pi/2 - np.arctan( 1.5 * M ) )
+                W = np.arctan( np.tan( S )**(1.0 /3.0 ) )
                 
-                # find true anomaly
-                sinv = ( np.sqrt( 1.0 -ecc*ecc ) * np.sin(E_1) ) / ( 1.0 -ecc*np.cos(E_1) )
-                cosv = ( np.cos(E_1)-ecc ) / ( 1.0  - ecc*np.cos(E_1) )
-                nu  = np.arctan2( sinv,cosv )
+                E = 2.0 *1.0/np.tan(2.0 *W)
+                
+                nu = 2.0  * np.arctan(E)
             else:
                 """
-                    CIRCULAR
+                    ELLIPTICAl
                 """
-                # -------------------- circular -------------------
-                count= 0
-                nu = M
-                E = M
-                
-            
-        
+                if  ecc > tol:   # elliptical logic
+                    
+                    # determine intial guess for iteration
+                    if M > -np.pi and (M < 0 or M > np.pi):
+                        E_0 = M - ecc
+                    else:
+                        E_0 = M + ecc
+                                        
+                    # newton's method iteration to find eccentric anomaly
+                    count= 1
+                    E_1 = E_0 + ( M - E_0 + ecc*np.sin(E_0) ) / ( 1.0  - ecc*np.cos(E_0) )
+                    while (( np.absolute(E_1-E_0) > tol ) and ( count <= max_iter )):
+                        count = count + 1
+                        E_0 = E_1
+                        E_1 = E_0 + ( M - E_0 + ecc*np.sin(E_0) ) / ( 1.0  - ecc*np.cos(E_0) )
+                    
+                    E = E_0
+                    
+                    # find true anomaly
+                    sinv = ( np.sqrt( 1.0 -ecc*ecc ) * np.sin(E_1) ) / ( 1.0 -ecc*np.cos(E_1) )
+                    cosv = ( np.cos(E_1)-ecc ) / ( 1.0  - ecc*np.cos(E_1) )
+                    nu  = np.arctan2( sinv,cosv )
+                else:
+                    """
+                        CIRCULAR
+                    """
+                    # -------------------- circular -------------------
+                    count= 0
+                    nu = M
+                    E = M
 
-    return (E,nu,count)
+    E_out.append(E)
+    nu_out.append(nu)
+    count_out.append(count)
+
+    return (np.squeeze(E_out), np.squeeze(nu_out), np.squeeze(count_out))
 
 def conic_orbit(p,ecc, inc, raan, arg_p, nu_i, nu_f):
     """Plot conic orbit
