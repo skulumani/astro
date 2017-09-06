30423893cc93041ba7da6c570c049b59bb82af4c
Shankar Kulumani <shanks.k@gmail.com>
2017-09-06 14:09:22 -0400

    Remove requirement for pyephem since it breaks windows

--- tle.py
+++ tle.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
-import ephem
+# import ephem
 from collections import namedtuple
 from spacetrack import SpaceTrackClient
 
@@ -103,22 +103,22 @@ def get_tle_spacetrack(filename, flag='all'):
         f.write(all_tles)
 
 
-def get_tle_ephem(filename):
-    """Load TLEs from a file
-    """
-    satlist = []
-    with open(filename, 'r') as f:
-        l1 = f.readline()
-        while l1:
-            l2 = f.readline()
-            l3 = f.readline()
-            sat = ephem.readtle(l1, l2, l3)
-            satlist.append(sat)
-            print(sat.name)
-            l1 = f.readline()
+# def get_tle_ephem(filename):
+#     """Load TLEs from a file
+#     """
+#     satlist = []
+#     with open(filename, 'r') as f:
+#         l1 = f.readline()
+#         while l1:
+#             l2 = f.readline()
+#             l3 = f.readline()
+#             sat = ephem.readtle(l1, l2, l3)
+#             satlist.append(sat)
+#             print(sat.name)
+#             l1 = f.readline()
 
-    print("{} satellites loaded into list".format(len(satlist)))
-    return satlist
+#     print("{} satellites loaded into list".format(len(satlist)))
+#     return satlist
 
 # write my own TLE parser since ephem doesn't save all the values
 
