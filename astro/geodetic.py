Fix my rotation matrix

astro/geodetic.py
@@ -97,7 +97,7 @@ def eci2ecef(jd):
 
     gst, _ = time.gstlst(jd, 0)
 
-    dcm = attitude.rot3(gst, 'c')
+    dcm = attitude.rot3(gst, 'r')
 
     return dcm
 
@@ -229,7 +229,7 @@ def rv2rhoazel(r_sat_eci, v_sat_eci, lat, lon, alt, jd):
     r_site_ecef = lla2ecef(lat, lon, alt)
 
     # convert sat and site to ecef
-    dcm_eci2ecef = eci2ecef(jd).T
+    dcm_eci2ecef = eci2ecef(jd)
     omega_earth = np.array([0, 0, constants.earth.omega])
 
     r_sat_ecef = dcm_eci2ecef.dot(r_sat_eci)

tests/test_geodetic.py
@@ -146,7 +146,8 @@ class TestECI2ECEF():
     _, lst = time.gstlst(jd_exp, lonexp)
     eci = geodetic.site2eci(latgdexp, altexp, lst)
     ecef = geodetic.lla2ecef(latgdexp, lonexp, altexp)
-    eci_from_ecef = geodetic.eci2ecef(jd_exp).dot(ecef)
+    Reci2ecef = geodetic.eci2ecef(jd_exp)
+    eci_from_ecef = Reci2ecef.T.dot(ecef)
 
     def test_eci_vallado(self):
         np.testing.assert_allclose(self.eci, self.eci_exp, rtol=1e-2)