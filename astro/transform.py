69ec26d8e00dce26ea9cc252b95c73b5baa9f491
Shankar Kulumani <shanks.k@gmail.com>
2017-11-11 16:29:40 -0500

    Working on adding transform functions

--- astro/transform.py
+++ astro/transform.py
@@ -3,7 +3,7 @@
 """
 
 import numpy as np
-from astro import kepler, constants
+from astro import kepler, constants, geodetic
 from kinematics import attitude
 
 # TODO: Documentation and unit testing
@@ -30,25 +30,64 @@ def dcm_pqw2eci_coe(raan, inc, arg_p):
 
     return dcm
 
-def dcm_sez2ecef(latgd, lon, alt):
-    pass
+#TODO: Documentation and testing
+def dcm_sez2ecef(latgd, lon, alt=0):
+    dcm = dcm_ecef2sez(latgd, lon)
+    return dcm.T
 
-def dcm_ecef2sez(latgd, lon, alt):
-    pass
+#TODO: Documentation and testing
+def dcm_ecef2sez(latgd, lon, alt=0):
+    dcm = attitude.rot2(np.pi/2 - latgd, 'r').dot(attitude.rot3(lon, 'r'))
+    return dcm
 
-def dcm_ned2ecef(latgd, lon, alt):
-    pass
+#TODO: Documentation and testing
+def dcm_ned2ecef(latgd, lon, alt=0):
+    dcm = dcm_ecef2ned(latgd, lon, alt)
+    return dcm.T
 
-def dcm_ecef2ned(latgd, lon, alt):
-    pass
+#TODO: Documentation and testing
+def dcm_ecef2ned(latgd, lon, alt=0):
+    dcm = np.array([[-np.sin(latgd)*np.cos(lon), -np.sin(lon), -np.cos(latgd)*np.cos(lon)],
+                    [-np.sin(latgd)*np.sin(lon), np.cos(lon), -np.cos(latgd)*np.sin(lon)],
+                    [np.cos(latgd), 0, -np.sin(latgd)]]).T
+    return dcm
 
-def dcm_ecef2enu(latgd, lon, alt):
+#TODO: Documentation and testing
+def dcm_ecef2enu(latgd, lon, alt=0):
     dcm = np.array([[-np.sin(lon), np.cos(lon), 0],
                     [-np.sin(latgd)*np.cos(lon), -np.sin(latgd)*np.sin(lon), np.cos(latgd)],
                     [np.cos(latgd)*np.cos(lon), np.cos(latgd)*np.sin(lon), np.sin(latgd)]])
 
     return dcm
 
-def dcm_enu2ecef(latgd, lon, alt):
+#TODO: Documentation and testing
+def dcm_enu2ecef(latgd, lon, alt=0):
     dcm = dcm_ecef2enu(latgd, lon, alt)
     return dcm.T
+
+# TODO: Add documentation and testing
+# TODO: Make this vectorized to allow for vector inputs
+def enu2ecefv(enu, latgd, lon, alt=0):
+
+    # find rotation matrix for this location enu2ecef
+    dcm_enu2ecef = dcm_enu2ecef(latgd, lon)
+
+    # vector to site (latgd, lon)
+    site_ecef = geodetic.lla2ecef(lat, lon, alt)
+
+    # transform
+    ecef = dcm_ecef2enu.dot(enu) + site_ecef
+
+    return ecef
+
+# TODO: Add documentation and testing
+# TODO: Make this vectorized to allow for vector inputs
+def ecef2enuv(ecef, latgd, lon, alt=0):
+    dcm_ecef2enu = dcm_ecef2enu(latgd, lon)
+
+    # site vector
+    site_ecef = geodetic.lla2ecef(lat, lon, alt)
+
+    # transform
+    enu = dcm_ecef2enu.dot(ecef - site_ecef)
+    return enu
