c4dd8ddd187799ac59bd871b0cdfb9805e739696
Shankar Kulumani <shanks.k@gmail.com>
2017-12-04 07:54:21 -0500

    Add a sun vec test

--- tests/test_planets.py
+++ tests/test_planets.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 
-from astro import planets
+from astro import planets, time
 import pdb
 
 
@@ -14,6 +14,19 @@ def test_sun_earth_eci_usafa():
     rsun, ra, dec = planets.sun_earth_eci(jd)
     np.testing.assert_allclose(rsun, expected_rsun, rtol=1e-4)
 
+def test_sun_eci_vallado():
+    jd, mjd = time.date2jd(1994, 4, 2, 0, 0, 0)
+    jd_vallado = 2449444.5
+    rsun_vallado = np.array([146241097,
+                             28574940,
+                             12389196])
+    ra_vallado, dec_vallado = np.deg2rad(11.056071), np.deg2rad(4.7529393)
+    rsun, ra, dec = planets.sun_earth_eci(jd)
+
+
+    np.testing.assert_allclose(jd, jd_vallado)
+    np.testing.assert_allclose(rsun, rsun_vallado, rtol=1e-4)
+    np.testing.assert_allclose((ra, dec), (ra_vallado, dec_vallado), rtol=1e-4)
 
 class TestMercuryCOE():
     jd = 2457928.5
