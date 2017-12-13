475208f4d8e904601bf7bb3cb2889bf6b2ec20ba
Shankar Kulumani <shanks.k@gmail.com>
2017-12-13 11:39:23 -0500

    Fix formatting of ascii art

--- astro/predict.py
+++ astro/predict.py
@@ -30,6 +30,7 @@ re = constants.earth.radius
 eesqrd = constants.earth.eesqrd
 ee = constants.earth.ee
 
+
 def predict(site_location, date_start, date_end, ifile='./tle.txt',
             ofile='./output.txt'):
     r"""PREDICT satellite passes for a given Earth location
@@ -74,31 +75,32 @@ def predict(site_location, date_start, date_end, ifile='./tle.txt',
     print("""
     ____________ ___________ _____ _____ _____ 
     | ___ \ ___ \  ___|  _  \_   _/  __ \_   _|
-    | |_/ / |_/ / |__ | | | | | | | /  \/ | |  
-    |  __/|    /|  __|| | | | | | | |     | |  
-    | |   | |\ \| |___| |/ / _| |_| \__/\ | |  
-    \_|   \_| \_\____/|___/  \___/ \____/ \_/  """)
+    | |_/ / |_/ / |__ | | | | | | | /  \/ | |
+    |  __/|    /|  __|| | | | | | | |     | |
+    | |   | |\ \| |___| |/ / _| |_| \__/\ | |
+    \_|   \_| \_\____/|___/  \___/ \____/ \_/
+          \n""")
 
     logger = logging.getLogger(__name__)
-                                           
+
     ifile = os.path.abspath(ifile)
     ofile = os.path.abspath(ofile)
 
     site_lat = np.deg2rad(site_location[0])
     site_lon = np.deg2rad(site_location[1])
     site_alt = site_location[2]
-    
+
     jd_start, _ = time.date2jd(date_start[0], date_start[1], date_start[2], date_start[3],
                                date_start[4], date_start[5])
     jd_end, _ = time.date2jd(date_end[0], date_end[1], date_end[2], date_end[3],
-                               date_end[4], date_end[5])
+                             date_end[4], date_end[5])
 
-    jd_step = 2 / (24 * 60) # step size in minutes
+    jd_step = 2 / (24 * 60)  # step size in minutes
     jd_span = np.arange(jd_start, jd_end, jd_step)
-    
+
     # build the site dictionary
     logger.info('Starting to create site dicitonary')
-    site = build_site(jd_span, np.deg2rad(site_location[0]), 
+    site = build_site(jd_span, np.deg2rad(site_location[0]),
                       np.deg2rad(site_location[1]), float(site_location[2]))
 
     # echo check some data
@@ -108,17 +110,17 @@ def predict(site_location, date_start, date_end, ifile='./tle.txt',
         f.write('Check Input Data : \n\n')
         f.write('Site Latitude    = {:9.6f} rad = {:9.6f} deg\n'.format(site_lat,
                                                                         np.rad2deg(site_lat)))
-        f.write('Site Longitude   = {:9.6f} rad = {:9.6f} deg\n'.format(site_lon, 
+        f.write('Site Longitude   = {:9.6f} rad = {:9.6f} deg\n'.format(site_lon,
                                                                         np.rad2deg(site_lon)))
-        f.write('Site Altitude    = {:9.6f} km  = {:9.6f} m\n'.format(site_alt, 
+        f.write('Site Altitude    = {:9.6f} km  = {:9.6f} m\n'.format(site_alt,
                                                                       site_alt * 1000))
-        
+
         f.write('\nObservation Window :\n\n')
-        f.write('Start Date : {} UTC\n'.format(datetime.datetime(int(date_start[0]), 
-                                                                 int(date_start[1]), 
-                                                                 int(date_start[2]), 
-                                                                 int(date_start[3]), 
-                                                                 int(date_start[4]), 
+        f.write('Start Date : {} UTC\n'.format(datetime.datetime(int(date_start[0]),
+                                                                 int(date_start[1]),
+                                                                 int(date_start[2]),
+                                                                 int(date_start[3]),
+                                                                 int(date_start[4]),
                                                                  int(date_start[5])).isoformat()))
         f.write('End Date   : {} UTC\n\n'.format(datetime.datetime(int(date_end[0]),
                                                                    int(date_end[1]),
@@ -126,23 +128,23 @@ def predict(site_location, date_start, date_end, ifile='./tle.txt',
                                                                    int(date_end[3]),
                                                                    int(date_end[4]),
                                                                    int(date_end[5])).isoformat()))
-        
+
         f.write('Start Julian Date   = {}\n'.format(jd_start))
         f.write('End Julian Date     = {}\n\n'.format(jd_end))
-
 
     # now we have to read the TLE
     logger.info('Read TLEs from {}'.format(ifile))
     sats = tle.get_tle(ifile)
-    logger.debug('Read {} sats'.format(len(sats))) 
+    logger.debug('Read {} sats'.format(len(sats)))
 
-    parallel_predict = functools.partial(satellite.parallel_predict, jd_span=jd_span, site=site)
-    
+    parallel_predict = functools.partial(
+        satellite.parallel_predict, jd_span=jd_span, site=site)
+
     logger.info('Starting parallel processing for satellites')
 
     with multiprocessing.Pool(multiprocessing.cpu_count() - 1) as p:
         sats_passes = p.map(parallel_predict, sats)
-    
+
     logger.info('Now outputting visible data to textfile')
 
     for sat, pass_vis in zip(sats, sats_passes):
@@ -152,10 +154,11 @@ def predict(site_location, date_start, date_end, ifile='./tle.txt',
     #     sat.tle_update(jd_span)
     #     sat.visible(site)
     #     sat.output(ofile)
-	
+
     print("Output saved to : \n{}".format(ofile))
 
-    return sats, sats_passes, site 
+    return sats, sats_passes, site
+
 
 def build_site(jd_span, site_lat, site_lon, site_alt):
     r"""Create the site dictionary
@@ -181,7 +184,7 @@ def build_site(jd_span, site_lat, site_lon, site_alt):
     Author
     ------
     Shankar Kulumani		GWU		skulumani@gwu.edu
-    """ 
+    """
     site_ecef = geodetic.lla2ecef(site_lat, site_lon, site_alt)
 
     # loop over jd span
@@ -212,6 +215,7 @@ def build_site(jd_span, site_lat, site_lon, site_alt):
 
     return site
 
+
 def parse_args(args):
     r"""Parse arguments from PREDICT 
 
@@ -229,14 +233,15 @@ def parse_args(args):
     Author
     ------
     Shankar Kulumani		GWU		skulumani@gwu.edu
-    """ 
+    """
 
     # default start and end times if none are given
     start_utc = list(datetime.datetime.today().timetuple()[0:6])
-    end_utc = list((datetime.datetime.today() + datetime.timedelta(days=7)).timetuple()[0:6])
-    
+    end_utc = list((datetime.datetime.today() +
+                    datetime.timedelta(days=7)).timetuple()[0:6])
+
     # default output filename
-    output_name = os.path.join(tempfile.gettempdir(),'predict_output.txt')
+    output_name = os.path.join(tempfile.gettempdir(), 'predict_output.txt')
 
     # parse inputs
     # TODO Figure out command line arguments which should be positional vs optional
@@ -244,16 +249,20 @@ def parse_args(args):
 
     parser = argparse.ArgumentParser(description='PREDICT satellite passes')
 
-    parser.add_argument('latitude', help='Latitude (deg) of Observation site', type=float)
-    parser.add_argument('longitude', help='Longitude (deg) of Observation site', type=float)
-    parser.add_argument('altitude', help='Altitude (km) of Observation site', type=float)
+    parser.add_argument(
+        'latitude', help='Latitude (deg) of Observation site', type=float)
+    parser.add_argument(
+        'longitude', help='Longitude (deg) of Observation site', type=float)
+    parser.add_argument(
+        'altitude', help='Altitude (km) of Observation site', type=float)
 
-    parser.add_argument('--start', '-s', help='UTC Space separated list of year month day of start of prediction window.', 
+    parser.add_argument('--start', '-s', help='UTC Space separated list of year month day of start of prediction window.',
                         default=start_utc, action='store', nargs=3, type=int)
-    parser.add_argument('--end', '-e', help='UTC Space separated list of year month day of end of prediction window.', 
+    parser.add_argument('--end', '-e', help='UTC Space separated list of year month day of end of prediction window.',
                         default=end_utc, action='store', nargs=3, type=int)
     # option to use saved tle
-    parser.add_argument('-i', '--input', help='Path to input tle file', action='store', type=str)
+    parser.add_argument(
+        '-i', '--input', help='Path to input tle file', action='store', type=str)
 
     parser.add_argument('-o', '--output', help='Path to output file', action='store', type=str,
                         default=output_name)
@@ -262,20 +271,20 @@ def parse_args(args):
 
     # option to download spacetrack tle and then run predict
     if not args.input:
-	# logging here
-        ifile = os.path.join(tempfile.gettempdir(),'predict_input.txt')
+        # logging here
+        ifile = os.path.join(tempfile.gettempdir(), 'predict_input.txt')
         tle.get_tle_visible(ifile)
     else:
-	# different logging
+        # different logging
         ifile = args.input
-    
+
     start_date = (args.start[0], args.start[1], args.start[2], 0, 0, 0)
     end_date = (args.end[0], args.end[1], args.end[2], 0, 0, 0)
 
     return (args.latitude, args.longitude, args.altitude), start_date, end_date, ifile, args.output
 
+
 if __name__ == "__main__":
-    site_location, start, end, input_file, output_file = parse_args(sys.argv[1:])
+    site_location, start, end, input_file, output_file = parse_args(
+        sys.argv[1:])
     predict(site_location, start, end, input_file, output_file)
-
-
